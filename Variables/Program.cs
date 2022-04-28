f4aad77b00d50cd585fff3fd8afa297c15a16a44
DeadCodeBoy <14.02.85@mail.ru>
2022-04-29 00:51:26 +0500

    task2

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hight;
+            float carSpeed;
+            double partOf;
+            bool yes;
+            long count;
+            short smallCount;
+            byte biteMe;
+            ulong countWithMinus;
+            char soundLikeChair;
+            decimal onlyNumbers;
+
+        }
+
+    }
+}
