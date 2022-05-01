972eb531b000c26960866bc51750d68af4443a45
DeadCodeBoy <14.02.85@mail.ru>
2022-05-01 12:49:30 +0500

    task 6

---
+++ task6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInLine = 3;
+            int allPictures = 52;
+            int maxOflines = allPictures / picturesInLine;
+            int lostPictures = allPictures % picturesInLine;
+            Console.WriteLine("полностью заполненных рядов можно вывести {0}, картинок сверх меры {1}", maxOflines, lostPictures);
+
+
+        }
+    }
+}
