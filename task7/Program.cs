4e3de5f92a46964b38923bb32eacf28912328e44
DeadCodeBoy <14.02.85@mail.ru>
2022-05-01 14:25:49 +0500

    task7

---
+++ task7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество золота");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            int cristallPrice = 5;
+            Console.WriteLine("Сколько кристаллов вы хотите купить, при цене {0} золота за один кристалл",cristallPrice);
+            int purchaseAmount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы успешно купили {0} кристаллов", purchaseAmount);
+            gold = gold- (purchaseAmount*cristallPrice);
+            Console.WriteLine("Золота у вас осталось {0}", gold);
+
+
+        }
+    }
+}
